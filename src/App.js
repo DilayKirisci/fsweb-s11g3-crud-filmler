7e8821fa52629d9819e169eaebb0445307e47f14
Dilay Kirisci <isik.kirisci@gmail.com>
2023-06-29 14:45:30 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -5,85 +5,122 @@ import MovieList from "./components/MovieList";
 import Movie from "./components/Movie";
 
 import MovieHeader from "./components/MovieHeader";
-import EditMovieForm from "./components/EditMovieForm";
-import AddMovieForm from "./components/AddMovieForm";
 
 import FavoriteMovieList from "./components/FavoriteMovieList";
 
 import axios from "axios";
+import EditMovieForm from "./components/EditMovieForm";
+import AddMovieForm from "./components/AddMovieForm";
 
 const App = (props) => {
-	const [movies, setMovies] = useState([]);
-	const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [theme, setTheme] = useState("light");
+  const history = useHistory();
 
-	const history = useHistory();
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
 
-	useEffect(() => {
-		axios
-			.get("http://localhost:9000/api/movies")
-			.then((res) => {
-				setMovies(res.data);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
 
-	const deleteMovie = (id) => {
-		axios
-			.delete("http://localhost:9000/api/movies/" + id)
-			.then((res) => {
-				const updatedMovies = movies.filter((m) => m.id !== id);
-				setMovies(updatedMovies);
-				console.log(movies);
-				history.push("/movies");
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
+  const themeToggle = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
-	const addToFavorites = (movie) => {};
+  console.log(theme);
 
-	return (
-		<div>
-			<nav className="bg-zinc-800 px-6 py-3">
-				<h1 className="text-xl text-white">HTTP / CRUD Film Projesi</h1>
-			</nav>
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/api/movies")
+      .then((res) => {
+        setMovies(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-			<div className="max-w-4xl mx-auto px-3 pb-4">
-				<MovieHeader />
-				<div className="flex flex-col sm:flex-row gap-4">
-					<FavoriteMovieList favoriteMovies={favoriteMovies} />
+  const deleteMovie = (id) => {
+    axios
+      .delete(`http://localhost:9000/api/movies/${id}`)
+      .then((res) => {
+        setMovies(res.data);
+        console.log(res.data);
+        history.push("/movies");
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
 
-					<Switch>
-						<Route path="/movies/edit/:id">
-							<EditMovieForm />
-						</Route>
+  const addToFavorites = (movie) => {
+    const alreadyInFavs = favoriteMovies.find((x) => movie.title === x.title);
+    if (!alreadyInFavs) {
+      const updatedFavorites = [...favoriteMovies, movie];
+      setFavoriteMovies(updatedFavorites);
+      history.push("/movies");
+    }
+  };
 
-						<Route path="/movies/add">
-							<AddMovieForm
-								setMovies={setMovies}
-								addToFavorites={addToFavorites}
-							/>
-						</Route>
+  return (
+    <div className=" dark:bg-black h-screen">
+      <nav className="bg-zinc-800 px-6 py-3 flex justify-between  items-center ">
+        <h1 className="text-xl text-white">HTTP / CRUD Film Projesi</h1>
+        <button
+          onClick={themeToggle}
+          className="p-1.5 text-sm  bg-stone-300 rounded-xl text-black "
+        >
+          Dark Mode
+        </button>
+      </nav>
 
-						<Route path="/movies/:id">
-							<Movie deleteMovie={deleteMovie} />
-						</Route>
+      <div className="max-w-4xl mx-auto px-3 pb-4">
+        <MovieHeader />
+        <div className="flex flex-col sm:flex-row gap-4">
+          <FavoriteMovieList favoriteMovies={favoriteMovies} />
 
-						<Route path="/movies">
-							<MovieList movies={movies} />
-						</Route>
+          <Switch>
+            <Route path="/movies/add">
+              <AddMovieForm
+                setMovies={setMovies}
+                addToFavorites={addToFavorites}
+              />
+            </Route>
 
-						<Route path="/">
-							<Redirect to="/movies" />
-						</Route>
-					</Switch>
-				</div>
-			</div>
-		</div>
-	);
+            <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies={setMovies} />
+            </Route>
+
+            <Route path="/movies/:id">
+              <Movie
+                deleteMovie={deleteMovie}
+                addToFavorites={addToFavorites}
+              />
+            </Route>
+
+            <Route path="/movies">
+              <MovieList movies={movies} />
+            </Route>
+
+            <Route path="/">
+              <Redirect to="/movies" />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,72 +4,75 @@ import { Link, useParams, useHistory } from "react-router-dom";
 import axios from "axios";
 
 const Movie = (props) => {
-	const { addToFavorites } = props;
+  const { addToFavorites, deleteMovie } = props;
 
-	const [movie, setMovie] = useState("");
+  const [movie, setMovie] = useState("");
 
-	const { id } = useParams();
-	const { push } = useHistory();
+  const { id } = useParams();
+  const { push } = useHistory();
 
-	useEffect(() => {
-		axios
-			.get(`http://localhost:9000/api/movies/${id}`)
-			.then((res) => {
-				setMovie(res.data);
-			})
-			.catch((err) => {
-				console.log(err.response);
-			});
-	}, [id]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:9000/api/movies/${id}`)
+      .then((res) => {
+        setMovie(res.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, [id]);
 
-	return (
-		<div className="bg-white rounded-md shadow flex-1">
-			<div className="p-5 pb-3 border-b border-zinc-200">
-				<h4 className="text-xl font-bold">{movie.title} Detayları</h4>
-			</div>
-			<div className="px-5 py-3">
-				<div className="py-1 flex">
-					<div className="view-label">İsim</div>
-					<div className="flex-1">{movie.title}</div>
-				</div>
-				<div className="py-1 flex">
-					<div className="view-label">Yönetmen</div>
-					<div className="flex-1">{movie.director}</div>
-				</div>
-				<div className="py-1 flex">
-					<div className="view-label">Tür</div>
-					<div className="flex-1">{movie.genre}</div>
-				</div>
-				<div className="py-1 flex">
-					<div className="view-label">Metascore</div>
-					<div className="flex-1">{movie.metascore}</div>
-				</div>
-				<div className="py-1 flex">
-					<div className="view-label">Açıklama</div>
-					<p className="flex-1">{movie.description}</p>
-				</div>
-			</div>
+  return (
+    <div className="bg-white rounded-md shadow flex-1  dark:bg-neutral-500  dark:text-white">
+      <div className="p-5 pb-3 border-b border-zinc-200">
+        <h4 className="text-xl font-bold">{movie.title} Detayları</h4>
+      </div>
+      <div className="px-5 py-3">
+        <div className="py-1 flex">
+          <div className="view-label">İsim</div>
+          <div className="flex-1">{movie.title}</div>
+        </div>
+        <div className="py-1 flex">
+          <div className="view-label">Yönetmen</div>
+          <div className="flex-1">{movie.director}</div>
+        </div>
+        <div className="py-1 flex">
+          <div className="view-label">Tür</div>
+          <div className="flex-1">{movie.genre}</div>
+        </div>
+        <div className="py-1 flex">
+          <div className="view-label">Metascore</div>
+          <div className="flex-1">{movie.metascore}</div>
+        </div>
+        <div className="py-1 flex">
+          <div className="view-label">Açıklama</div>
+          <p className="flex-1">{movie.description}</p>
+        </div>
+      </div>
 
-			<div className="px-5 py-3 border-t border-zinc-200 flex justify-end gap-2">
-				<button className="myButton bg-blue-600 hover:bg-blue-500 ">
-					Favorilere ekle
-				</button>
-				<Link
-					to={`/movies/edit/${movie.id}`}
-					className="myButton bg-blue-600 hover:bg-blue-500"
-				>
-					Edit
-				</Link>
-				<button
-					type="button"
-					className="myButton bg-red-600 hover:bg-red-500"
-					onClick={() => props.deleteMovie(movie.id)}
-				>
-					Sil
-				</button>
-			</div>
-		</div>
-	);
+      <div className="px-5 py-3 border-t border-zinc-200 flex justify-end gap-2">
+        <button
+          onClick={() => addToFavorites({ ...movie, id: id })}
+          className="myButton bg-blue-600 hover:bg-blue-500  dark:bg-slate-700 "
+        >
+          Favorilere ekle
+        </button>
+        <Link
+          to={`/movies/edit/${movie.id}`}
+          className="myButton bg-blue-600 hover:bg-blue-500  dark:bg-slate-700"
+        >
+          Edit
+        </Link>
+        <button
+          onClick={() => deleteMovie(id)}
+          type="button"
+          className="myButton bg-red-600 hover:bg-red-500  dark:bg-red-900"
+        >
+          Sil
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Movie;
